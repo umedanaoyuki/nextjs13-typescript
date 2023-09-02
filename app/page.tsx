67162ba71a19d9268fd5562c7ba36d5d0f6c33aa
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-02 16:08:33 +0900

    initial commit(ルーティングの勉強)

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+      <div>
+          <h1>新着記事</h1>
+          <ul>
+              <li>記事1</li>
+              <li>記事2</li>
+              <li>記事3</li>
+          </ul>
+      </div>
+  )
+}
